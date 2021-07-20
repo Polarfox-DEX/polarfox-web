273454eeb178cea1be2eba7a7a744a06ebf5248f
Clément Aguilar <aguilar.clement@outlook.fr>
2021-07-20 07:01:02 +0200

    Merge master into products

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,14 @@
-import classNames from 'classnames';
-import Link from 'next/link';
-import { calcRem } from '../utils/styles';
-import { SectionProps } from './sections/utils/SectionProps';
+import classNames from "classnames";
+import Link from "next/link";
+import Analytics from "./svg/Analytics";
+import Bridge from "./svg/Bridge";
+import DoubleArrow from "./svg/DoubleArrow";
+import MenuArrow from "./svg/MenuArrow";
+import Mining from "./svg/Mining";
+import TopRightArrow from "./svg/TopRightArrow";
+import { calcRem } from "../utils/styles";
+import { SectionProps } from "./utils/SectionProps";
+import { ReactNode } from "react";
 
 export function Header({ className }: SectionProps) {
   return (
@@ -23,7 +30,14 @@ export function Header({ className }: SectionProps) {
           lineHeight: calcRem(16.45),
         }}
       >
-        <div>Products</div>
+        <div
+          className="rounded-3xl bg-gray-mid2 flex items-center justify-between -mr-5 px-4"
+          style={{ width: calcRem(110), height: calcRem(44) }}
+        >
+          Products
+          <MenuArrow />
+        </div>
+        <ProductsDropdown isActive />
         <Link href="/about">
           <a>About</a>
         </Link>
@@ -32,7 +46,128 @@ export function Header({ className }: SectionProps) {
           <a>FAQ</a>
         </Link>
         <div>Resources</div>
+        <div>Language</div>
+        <div>Account</div>
       </div>
+    </div>
+  );
+}
+
+interface ProductsDropdownProps {
+  isActive?: boolean;
+  className?: string;
+}
+
+function ProductsDropdown({ isActive, className }: ProductsDropdownProps) {
+  return isActive ? (
+    <div
+      className="bg-white rounded-xl absolute flex justify-between space-x-3 px-4 py-6"
+      style={{
+        width: calcRem(779),
+        height: calcRem(219),
+        marginTop: calcRem(300),
+        marginLeft: calcRem(-16),
+      }}
+    >
+      <Product
+        logo={<DoubleArrow />}
+        href="https://dex.polarfox.io"
+        linkTitle="Open app"
+        textClass="mt-11"
+      >
+        Decentralized Exchange
+      </Product>
+      <Product
+        logo={<Analytics />}
+        href="https://analytics.polarfox.io"
+        linkTitle="Open app"
+      >
+        Analytics
+      </Product>
+      <Product
+        logo={<Mining />}
+        href="https://dex.polarfox.io/#/pfx"
+        linkTitle="Open app"
+      >
+        Mining Pools
+      </Product>
+      <Product
+        logo={<Bridge />}
+        href="https://bridge.polarfox.io"
+        linkTitle="Open app"
+      >
+        Bridge
+      </Product>
+      <Product
+        logo={
+          <img
+            src="akita.png"
+            alt="akita"
+            style={{ height: calcRem(24), width: calcRem(24) }}
+          />
+        }
+        href="https://akita.network"
+        linkTitle="Visit site"
+      >
+        AKITA Network
+      </Product>
+    </div>
+  ) : (
+    <div className="absolute" />
+  );
+}
+
+interface ProductProps {
+  logo: ReactNode;
+  href: string;
+  linkTitle: string;
+  textClass?: string;
+  className?: string;
+  children: ReactNode;
+}
+
+function Product({
+  logo,
+  href,
+  linkTitle,
+  textClass,
+  className,
+  children,
+}: ProductProps) {
+  return (
+    <div style={{ width: calcRem(137) }}>
+      <Link href={href}>
+        <a>
+          <div
+            className="rounded-xl hover:bg-gray-dark mx-3 px-3 pt-4"
+            style={{
+              height: calcRem(121),
+            }}
+          >
+            {logo}
+            <div
+              className={classNames("font-semibold", textClass || "mt-14")}
+              style={{ fontSize: calcRem(12), lineHeight: calcRem(14) }}
+            >
+              {children}
+            </div>
+          </div>
+        </a>
+      </Link>
+      <Link href={href}>
+        <a
+          className="border border-blue rounded-xl border-opacity-10 mt-2 flex items-center justify-between font-semibold pl-4"
+          style={{
+            height: calcRem(40),
+          }}
+        >
+          {linkTitle}
+          <TopRightArrow
+            className="fill-current mx-4"
+            style={{ height: calcRem(7), width: calcRem(7) }}
+          />
+        </a>
+      </Link>
     </div>
   );
 }
--- components/faq/FaqPreview.tsx
+++ components/faq/FaqPreview.tsx
@@ -1,62 +1,75 @@
-import classNames from 'classnames';
-import { calcRem } from '../../utils/styles';
-import { Question } from './Question';
-import { SectionProps } from '../sections/utils/SectionProps';
-import { SectionHeading } from '../sections/utils/SectionHeading';
-import { SectionTitle } from '../sections/utils/SectionTitle';
-import { SectionButton } from '../sections/utils/SectionButton';
+import classNames from "classnames";
+import Link from "next/link";
+import { calcRem } from "../../utils/styles";
+import { Question } from "./Question";
+import { SectionProps } from "../sections/utils/SectionProps"
 
 export function FaqPreview({ className }: SectionProps) {
   return (
-    <div
-      className={classNames(
-        'container flex flex-wrap justify-between',
-        className
-      )}
-    >
+    <div className={classNames("container flex justify-between", className)}>
       <div>
-        <SectionHeading>FAQ</SectionHeading>
-        <SectionTitle className="mt-1 mb-12">
+        <div
+          className="opacity-50"
+          style={{
+            fontSize: calcRem(12),
+            lineHeight: calcRem(14.1),
+            letterSpacing: calcRem(3),
+          }}
+        >
+          FAQ
+        </div>
+        <h2
+          className="font-switzer font-semibold mt-1"
+          style={{
+            maxWidth: calcRem(357),
+            fontSize: calcRem(50),
+            lineHeight: calcRem(65),
+          }}
+        >
           Frequently Asked Questions
-        </SectionTitle>
-        <SectionButton href="/faq">More questions</SectionButton>
+        </h2>
+        <button
+          className="mt-12 rounded-full border border-blue bg-white font-semibold"
+          style={{ width: calcRem(166), height: calcRem(44) }}
+        >
+          <Link href="/faq">
+            <a>More questions</a>
+          </Link>
+        </button>
       </div>
 
-      <div
-        className="divide-y divide-gray-light mt-16 desktop:mt-0"
-        style={{ width: calcRem(680) }}
-      >
+      <div className="pl-44 divide-y divide-gray-light">
         <Question question="What is Polarfox?" isOpen={true}>
-          Polarfox is a crypto ecosystem, based on Avalanche, which will bring
-          many apps together. Its main app is the Polarfox DEX; some other apps
-          have already been added to the ecosystem, such as the Polarfox bridge,
-          and many others are coming. Stay tuned! It aims to be fully
-          open-source and decentralized. Polarfox’s main values are trust,
-          honesty and transparency.
+            Polarfox is a crypto ecosystem, based on Avalanche, which will bring
+            many apps together. Its main app is the Polarfox DEX; some other
+            apps have already been added to the ecosystem, such as the Polarfox
+            bridge, and many others are coming. Stay tuned! It aims to be fully
+            open-source and decentralized. Polarfox’s main values are trust,
+            honesty and transparency.
         </Question>
         <Question question="What is the Polarfox DEX?" isOpen={false}>
-          Polarfox is a crypto ecosystem, based on Avalanche, which will bring
-          many apps together. Its main app is the Polarfox DEX; some other apps
-          have already been added to the ecosystem, such as the Polarfox bridge,
-          and many others are coming. Stay tuned! It aims to be fully
-          open-source and decentralized. Polarfox’s main values are trust,
-          honesty and transparency.
+            Polarfox is a crypto ecosystem, based on Avalanche, which will bring
+            many apps together. Its main app is the Polarfox DEX; some other
+            apps have already been added to the ecosystem, such as the Polarfox
+            bridge, and many others are coming. Stay tuned! It aims to be fully
+            open-source and decentralized. Polarfox’s main values are trust,
+            honesty and transparency.
         </Question>
         <Question question="What is PFX?" isOpen={false}>
-          Polarfox is a crypto ecosystem, based on Avalanche, which will bring
-          many apps together. Its main app is the Polarfox DEX; some other apps
-          have already been added to the ecosystem, such as the Polarfox bridge,
-          and many others are coming. Stay tuned! It aims to be fully
-          open-source and decentralized. Polarfox’s main values are trust,
-          honesty and transparency.
+            Polarfox is a crypto ecosystem, based on Avalanche, which will bring
+            many apps together. Its main app is the Polarfox DEX; some other
+            apps have already been added to the ecosystem, such as the Polarfox
+            bridge, and many others are coming. Stay tuned! It aims to be fully
+            open-source and decentralized. Polarfox’s main values are trust,
+            honesty and transparency.
         </Question>
         <Question question="What are the PFX tokenomics?" isOpen={false}>
-          Polarfox is a crypto ecosystem, based on Avalanche, which will bring
-          many apps together. Its main app is the Polarfox DEX; some other apps
-          have already been added to the ecosystem, such as the Polarfox bridge,
-          and many others are coming. Stay tuned! It aims to be fully
-          open-source and decentralized. Polarfox’s main values are trust,
-          honesty and transparency.
+            Polarfox is a crypto ecosystem, based on Avalanche, which will bring
+            many apps together. Its main app is the Polarfox DEX; some other
+            apps have already been added to the ecosystem, such as the Polarfox
+            bridge, and many others are coming. Stay tuned! It aims to be fully
+            open-source and decentralized. Polarfox’s main values are trust,
+            honesty and transparency.
         </Question>
       </div>
     </div>
