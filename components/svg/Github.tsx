f446f2c628326dafd16c89b3e437c1639baa6595
Clément Aguilar <aguilar.clement@outlook.fr>
2021-07-21 14:04:31 +0200

    Readd props.color

--- components/svg/Github.tsx
+++ components/svg/Github.tsx
@@ -4,7 +4,7 @@ function GitHub(props: React.SVGProps<SVGSVGElement>) {
       width="1em"
       height="1em"
       viewBox="0 0 22 22"
-      fill="#001937"
+      fill={props.color ?? '#001937'}
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
