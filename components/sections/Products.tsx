06cbc7e2d16c492c5f402867700e2c3e1cf273e7
Clément Aguilar <aguilar.clement@outlook.fr>
2021-07-21 13:29:58 +0200

    Start adding hover effect + analytics product

--- components/sections/Products.tsx
+++ components/sections/Products.tsx
@@ -1,8 +1,13 @@
-import classNames from 'classnames';
-import { calcRem } from '../../utils/styles';
-import TopRightArrow from '../svg/TopRightArrow';
-import { SectionHeading } from './utils/SectionHeading';
-import { SectionTitle } from './utils/SectionTitle';
+import classNames from "classnames";
+import AnalyticsGradient from "../svg/AnalyticsGradient";
+import BridgeGradient from "../svg/BridgeGradient";
+import DoubleArrowGradient from "../svg/DoubleArrowGradient";
+import TokenGradient from "../svg/TokenGradient";
+import TopRightArrow from "../svg/TopRightArrow";
+import { calcRem } from "../../utils/styles";
+import { ReactNode } from "react";
+import { SectionHeading } from "./utils/SectionHeading";
+import { SectionTitle } from "./utils/SectionTitle";
 
 interface ProductsSectionProps {
   className?: string;
@@ -10,20 +15,39 @@ interface ProductsSectionProps {
 
 export function Products({ className }: ProductsSectionProps) {
   return (
-    <div className={classNames('container', className)}>
+    <div className={classNames("container", className)}>
       <SectionHeading>PFX PORTFOLIO</SectionHeading>
       <SectionTitle className="mt-1">Products</SectionTitle>
       <div className="flex flex-wrap justify-between gap-y-5 mt-16 desktop:mt-28">
         <Product
           label="Decentralized Exchange"
           url="https://dex-test.polarfox.io/"
+          logo={<DoubleArrowGradient />}
         />
-        <Product label="Polarfox Token (PFX)" url="#" />
+        <Product
+          label="Polarfox Analytics"
+          url="https://analytics-test.polarfox.io"
+          logo={<AnalyticsGradient />}
+        />
+        <Product
+          label="Polarfox Token (PFX)"
+          url="#"
+          logo={<TokenGradient />}
+        />
         <Product
           label="Polarfox Bridge"
           url="https://bridge-test.polarfox.io"
+          logo={<BridgeGradient />}
         />
-        <Product label="Akita Community" url="https://akita.network" />
+        <Product
+          label="Akita Community"
+          url="https://akita.network"
+          logo={
+            <div className="-mt-1 hidden group-hover:block">
+            <img src="/akita-gradient.png" alt="akita" width="60" height="60" />
+            </div>
+          }
+        />
       </div>
     </div>
   );
@@ -32,37 +56,42 @@ export function Products({ className }: ProductsSectionProps) {
 interface ProductProps {
   label: string;
   url: string;
+  logo: ReactNode;
 }
 
-function Product({ label, url }: ProductProps) {
+function Product({ label, url, logo }: ProductProps) {
   return (
     <a
       href={url}
       target="_blank"
       rel="noopener noreferrer"
-      className="product w-full flex p-7 bg-white border border-blue"
+      className="product w-full flex justify-between p-7 bg-white border border-blue hover:bg-blue-light hover:border-blue-light hover:text-white group"
     >
-      <p
-        className="flex-1 self-end text-lg font-switzer font-semibold"
-        style={{
-          width: calcRem(224),
-          fontSize: calcRem(20),
-          lineHeight: calcRem(26.4),
-        }}
-      >
-        {label}
-      </p>
-      <div className="text-right">
+      <div className="flex flex-wrap">
+        <div className="-mt-1">{logo}</div>
+        <p
+          className=" self-end absolute font-switzer font-semibold"
+          style={{
+            width: calcRem(224),
+            fontSize: calcRem(20),
+            lineHeight: calcRem(26.4),
+          }}
+        >
+          {label}
+        </p>
+      </div>
+      <div className="a">
         <TopRightArrow className="fill-current" />
       </div>
       <style jsx>{`
         .product {
-          height: ${calcRem(270)};
+          height: ${calcRem(195)};
           border-radius: ${calcRem(10)};
         }
 
         @media (min-width: 1200px) {
           .product {
+            height: ${calcRem(270)};
             max-width: ${calcRem(285)};
           }
         }
---
+++ components/svg/BridgeGradient.tsx
@@ -0,0 +1,32 @@
+export function BridgeGradient(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="60"
+      height="60"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M60 3.32571V23.3314C59.9785 24.2083 59.6217 25.0435 59.0031 25.6654C58.3844 26.2872 57.551 26.6482 56.6743 26.6743H38.0571L26.6743 38.0571V56.6743C26.6482 57.551 26.2872 58.3844 25.6654 59.0031C25.0435 59.6217 24.2083 59.9785 23.3314 60H3.32571C2.45186 59.9742 1.62089 59.6155 1.00271 58.9973C0.384528 58.3791 0.025845 57.5481 0 56.6743V36.6686C0.0215411 35.7917 0.378325 34.9565 0.996947 34.3346C1.61557 33.7128 2.44898 33.3518 3.32571 33.3257H21.9429L33.3257 21.9429V3.32571C33.3518 2.44898 33.7128 1.61557 34.3346 0.996947C34.9565 0.378325 35.7917 0.0215411 36.6686 0H56.6743C57.5481 0.025845 58.3791 0.384528 58.9973 1.00271C59.6155 1.62089 59.9742 2.45186 60 3.32571Z"
+        fill="url(#paint0_linear)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear"
+          x1="30"
+          y1="0"
+          x2="30"
+          y2="60"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default BridgeGradient;
