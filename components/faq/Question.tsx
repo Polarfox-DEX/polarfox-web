46ed4188e6f085b6f72b30e1d3e676cb1faf9b4a
Clément Aguilar <aguilar.clement@outlook.fr>
2021-08-05 11:02:50 +0200

    Fix <ul> cannot be descendant of <p> error

--- components/faq/Question.tsx
+++ components/faq/Question.tsx
@@ -30,9 +30,12 @@ export function Question({ question, isOpenDefault, children }: QuestionProps) {
         </button>
       </h3>
       {isOpen && (
-        <p className="mt-3 mb-8 text-gray" style={{ lineHeight: calcRem(30) }}>
+        <div
+          className="mt-3 mb-8 text-gray"
+          style={{ lineHeight: calcRem(30) }}
+        >
           {children}
-        </p>
+        </div>
       )}
     </div>
   )
