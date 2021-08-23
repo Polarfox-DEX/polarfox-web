3fb9df80abccdf86693d8ad7a26102d89ddad9d0
BowgartField <BowgartField@users.noreply.github.com>
2021-08-23 17:13:24 +0200

    Add totalSoldInUsd and Level maximum

--- pages/web3.tsx
+++ pages/web3.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react"
 import Web3 from 'web3';
-import { utils } from 'web3'
 import { Page } from '../components/Page'
 
 export enum ChaindId{
@@ -31,6 +30,7 @@ export default function PresaleWeb3App(){
     const[currentLevelSoldAmountUsd,setCurrentLevelSoldAmountUsd] = useState(0)
     const[currentLevel,setCurrentLevel] = useState(0)
     const[bnbUsdPrice,setBnbUsdPrice] = useState(0)
+    const[totalSoldInBNB,setTotalSoldInBNB] = useState(0)
 
     //Connect website to user's metamask and get address
     var connect = async (): Promise<void> => {
@@ -92,12 +92,15 @@ export default function PresaleWeb3App(){
             setCurrentLevelSoldAmountUsd(parseFloat(web3.utils.fromWei(currentLevelSoldAmountUsd.toString())))
         )
         polarfoxTokenSaleContract.methods.currentBnbPrice().call().then((bnbPrice: number) => setBnbUsdPrice(bnbPrice))
+        window.ethereum.request({method: 'eth_getBalance', params: [contractAddress]}).then((balance: string) => setTotalSoldInBNB(parseFloat(web3.utils.fromWei(balance))))
+
+        addEventsListener()
 
     }
 
     var buy = async () => {
 
-        const BNBvalueInWei = utils.toWei(userAllocation)
+        const BNBvalueInWei = web3.utils.toWei(userAllocation)
 
         window.ethereum.request({method: 'eth_gasPrice', params: []}).then(async (gasFees: number) => {
 
@@ -134,22 +137,34 @@ export default function PresaleWeb3App(){
 
     var getLevelPrice = (level: number) => {
 
-        const levelPrice: number[] = [
-            1.8, 1.89, 1.98, 2.07, 2.16, 2.25, 2.34, 2.43, 2.52, 2.61,
-            2.7, 2.79, 2.88, 2.97, 3.06, 3.15, 3.24, 3.33, 3.42, 3.51,
-            3.6, 3.69, 3.78, 3.87, 3.96, 4.05, 4.14, 4.23, 4.32, 4.41,
-            4.5, 4.59, 4.68, 4.77, 4.86, 4.95, 5.04, 5.13, 5.22, 5.31,
-            5.4, 5.49, 5.58, 5.67, 5.76, 5.85, 5.94, 6.03, 6.12, 6.21,
-            6.3, 6.39, 6.48, 6.57, 6.66, 6.75, 6.84, 6.93, 7.02, 7.11,
-            7.2, 7.29, 7.38, 7.47, 7.56, 7.65, 7.74, 7.83, 7.92, 8.01,
-            8.1, 8.19, 8.28, 8.37, 8.46, 8.55, 8.64, 8.73, 8.82, 8.91,
-            9, 9.09, 9.18, 9.27, 9.36, 9.45, 9.54, 9.63, 9.72, 9.81,
-            9.9, 9.99, 10.8, 11.61, 12.42, 13.23, 14.04, 14.85, 16.56, 17.37,
-            18, 20.25, 22.05, 23.49, 24.48, 25.29, 26.1, 27, 27.9, 28.8,
-            29.7, 30.6, 31.5, 32.4, 33.3, 34.2, 35.1, 36, 36.9, 37.8
+        const levelPrice: number[][] = [
+            [100000, 1.8],[100000, 1.89],[100000, 1.98],[100000, 2.07],[100000, 2.16],[100000, 2.25],[100000, 2.34],[100000, 2.43],[100000, 2.52],[100000, 2.61],[100000, 2.7],
+            [100000, 2.79],[100000, 2.88],[100000, 2.97],[100000, 3.06],[100000, 3.15],[100000, 3.24],[100000, 3.33],[100000, 3.42],[100000, 3.51],[100000, 3.6],[100000, 3.69],
+            [250000, 3.78],[250000, 3.87],[250000, 3.96],[250000, 4.05],[250000, 4.14],[250000, 4.23],[250000, 4.32],[250000, 4.41],[250000, 4.5],[250000, 4.59],[250000, 4.68],
+            [250000, 4.77],[250000, 4.86],[250000, 4.95],[250000, 5.04],[250000, 5.13],[250000, 5.22],[250000, 5.31],[250000, 5.4],[250000, 5.49],[250000, 5.58],[250000, 5.67],
+            [250000, 5.76],[250000, 5.85],[250000, 5.94],[250000, 6.03],[250000, 6.12],[250000, 6.21],[250000, 6.3],[250000, 6.39],[500000, 6.48],[500000, 6.57],[500000, 6.66],
+            [500000, 6.75],[500000, 6.84],[500000, 6.93],[500000, 7.02],[500000, 7.11],[500000, 7.2],[500000, 7.29],[500000, 7.38],[500000, 7.47],[500000, 7.56],[500000, 7.65],
+            [500000, 7.74],[500000, 7.83],[500000, 7.92],[500000, 8.01],[500000, 8.1],[500000, 8.19],[500000, 8.28],[500000, 8.37],[500000, 8.46],[500000, 8.55],[500000, 8.64],
+            [500000, 8.73],[500000, 8.82],[500000, 8.91],[500000, 9],[500000, 9.09],[500000, 9.18],[500000, 9.27],[500000, 9.36],[500000, 9.45],[500000, 9.54],[500000, 9.63],
+            [500000, 9.72],[500000, 9.81],[500000, 9.9],[500000, 9.99],[535714, 10.8],[570796, 11.61],[1210526, 12.42],[1278260, 13.23],[1344827, 14.04],[1410256, 14.85],
+            [1559322, 16.56],[2764446, 17.37],[1057142.26, 18],[1189285.04, 20.25],[1294999.26, 22.05],[1379570.64, 23.49],[2156570.20, 24.48],[2227927.30, 25.29],[2299284.40, 26.1],
+            [2378570.07, 27],[2457855.74, 27.9],[2537141.41, 28.8],[2616427.08, 29.7],[2695712.75, 30.6],[2774998.42, 31.5],[2854284.09, 32.4],[2933569.76, 33.3],[3012855.43, 34.2],
+            [3092141.10, 35.1],[3171426.77, 36],[3250712.43, 36.9],[3330031.42, 37.8]
         ];
 
         return levelPrice[level]
+
+    }
+
+    var addEventsListener = () => {
+
+        /* 
+        polarfoxTokenSaleContract.events.Sold().on('data', (data: any) => {
+
+            console.log(data)
+
+        })
+        */
 
     }
 
@@ -162,9 +177,12 @@ export default function PresaleWeb3App(){
 
                 <hr/>
 
+                <label>Sold total: {totalSoldInBNB*bnbUsdPrice}$</label>
+                <br/>
+
                 <label>Current level: {currentLevel+1}/120</label><br/>
-                <label>PFX level price: {getLevelPrice(currentLevel)}$</label><br/>
-                <label>Level Progression: {currentLevelSoldAmountUsd}$</label><br/>
+                <label>PFX level price: {getLevelPrice(currentLevel)[1]}</label><br/>
+                <label>Level Progression: {currentLevelSoldAmountUsd}/{getLevelPrice(currentLevel)[0]}$</label><br/>
 
                 <hr/>
 
