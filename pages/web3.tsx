69cbabc776b95351cad0f88d6c374418a6b0355e
BowgartField <BowgartField@users.noreply.github.com>
2021-08-23 15:49:46 +0200

    First commit

--- pages/web3.tsx
+++ pages/web3.tsx
@@ -1,54 +1,71 @@
-import { useEffect, useState } from "react"
-import Web3 from 'web3'
+import React, { useEffect, useState } from "react"
+import Web3 from 'web3';
+import { utils } from 'web3'
+import { Page } from '../components/Page'
 
-const mainnetChainID = 56 //Binance Smart Chain Mainnet ID
-const testnetChainID = 97
-const bscMainnetURL = "https://bsc-dataseed1.binance.org:443"
-const bscTestnetURL = "https://bsc-dataseed1.binance.org:443"
+export enum ChaindId{
+    BSC_MAINNET = 56,
+    BSC_TESTNET = 97
+}
 
-var web3 = new Web3(bscTestnetURL);
+const { abi } = require('../../polarfox-presale/artifacts/contracts/PolarfoxTokenSale.sol/PolarfoxTokenSale.json');
+const contractAddress = "0x765547f8CeCad11fa122C61Fca1bc7e79b062336"
 
-const { abi } = require('../../polarfox-presale/build/PolarfoxTokenSale.json');
-const contractAddress = "0x042999BE6E7a7D8bcDfedeC473D77DC0dd83857a"
-let contract = new web3.eth.Contract(abi,contractAddress)
+declare let window: any;
 
+var web3: Web3;
+var polarfoxTokenSaleContract: any;
 
-export default function web3App(){
+export default function PresaleWeb3App(){
 
-    const[address,setAddress] = useState("");
     const[isLoading,setIsLoading] = useState(false)
 
-    const[totalValue,setTotalValue] = useState(0)
+    const[address,setAddress] = useState("");
+    const[connected,setConnected] = useState(false)
+    const[userBalance,setUserBalance] = useState("")
+
+    const[userAllocation,setUserAllocation] = useState('')
+    const[PFXtoReceive,setPFXtoReceive] = useState(0)
+    const[referrerAddress,setReferrerAddress] = useState('')
+
+    const[currentLevelSoldAmountUsd,setCurrentLevelSoldAmountUsd] = useState(0)
     const[currentLevel,setCurrentLevel] = useState(0)
-    const[totalCurrentLevel,setTotalCurrentLevel] = useState(0)
-
-    useEffect(() => {
-
-        //initialize App
-        getContratInfo();
-        addEventListeners();
-
-    })
+    const[bnbUsdPrice,setBnbUsdPrice] = useState(0)
 
     //Connect website to user's metamask and get address
     var connect = async (): Promise<void> => {
 
-        if(ethereum != undefined){
+        //Verify user has metamask
+        if(window.ethereum != undefined){
 
-            await ethereum.request({ method: 'eth_chainId' })
+            web3 = new Web3(window.ethereum)
+
+            initializePresaleContract();
+            
+            await window.ethereum.request({ method: 'eth_chainId' })
                 .then(async (userChainID: string) => {
 
-                    if(web3.utils.hexToNumber(userChainID) == mainnetChainID || 
-                        web3.utils.hexToNumber(userChainID) == testnetChainID){
-        
-                        await ethereum.request({ method: 'eth_requestAccounts' })
-                            .then((accounts: string[]) => {
+                    //Verify if metamask is connected to BSC
+                    if(web3.utils.hexToNumber(userChainID) == ChaindId.BSC_TESTNET || web3.utils.hexToNumber(userChainID) == ChaindId.BSC_MAINNET){
 
-                                const account = accounts[0]
-                                console.log(account)
-                                setAddress(account)
+                        //Request wallet connexion
+                        await window.ethereum.request({ method: 'eth_requestAccounts' })
+                            .then(async (accounts: string[]) => {
 
-                            }).catch((err) => {
+                                setAddress(accounts[0])
+
+                                //Request user wallet's balance
+                                await window.ethereum.request({method: 'eth_getBalance', params:[accounts[0]]})
+                                    .then((userBalance: number) => {
+
+                                        setUserBalance(web3.utils.fromWei(userBalance.toString()))
+                                        setConnected(true)
+
+                                    }).catch((err: any) => {
+                                        console.log(err)
+                                    })
+
+                            }).catch((err: any) => {
                                 console.log(err)
                             })
                         
@@ -56,7 +73,7 @@ export default function web3App(){
                         alert('Change network to Binance Smart Chain')
                     }
 
-                }).catch((error) => {
+                }).catch((error: any) => {
                     console.log(error)
                 })
 
@@ -66,51 +83,114 @@ export default function web3App(){
 
     }
 
-    var buy = () => {
+    var initializePresaleContract = () => {
+
+        polarfoxTokenSaleContract = new web3.eth.Contract(abi,contractAddress)
+        
+        polarfoxTokenSaleContract.methods.currentLevel().call().then((currentLevel: number) => setCurrentLevel(currentLevel))
+        polarfoxTokenSaleContract.methods.currentSoldAmountUsd().call().then((currentLevelSoldAmountUsd: number) => 
+            setCurrentLevelSoldAmountUsd(parseFloat(web3.utils.fromWei(currentLevelSoldAmountUsd.toString())))
+        )
+        polarfoxTokenSaleContract.methods.currentBnbPrice().call().then((bnbPrice: number) => setBnbUsdPrice(bnbPrice))
 
     }
 
-    var getContratInfo = async (): Promise<void> => {
+    var buy = async () => {
 
-        setIsLoading(true)
+        const BNBvalueInWei = utils.toWei(userAllocation)
 
-        //total of Presale
-        //total of current level
-        //current level
-        //
+        window.ethereum.request({method: 'eth_gasPrice', params: []}).then(async (gasFees: number) => {
 
-        //can buy
+            console.log(polarfoxTokenSaleContract)
 
+            await polarfoxTokenSaleContract.methods.buyTokens().send({
 
+                from: address,
+                value: BNBvalueInWei,
+                gasPrice: gasFees
+    
+            }).then((data: any) => {
+                console.log(data)
+            }).catch((error: any) => {
+                console.log(error)
+            })
 
+        })
 
     }
 
-    var addEventListeners = () => {
+    var userAllocationChange = async (value: string) => {
 
-        ethereum.on('chainChanged', (chainId) => {
-            //reload application when chain is changed
-            window.location.reload();
-        });
+        setUserAllocation(value.replace(",","."));
+        setPFXtoReceive((parseFloat(value)*bnbUsdPrice)/getLevelPrice(currentLevel))
+
+    }
+
+    var setMaxAllocation = () => {
+
+        setUserAllocation(userBalance.toString());
+        userAllocationChange(userBalance.toString());
+    }
+
+    var getLevelPrice = (level: number) => {
+
+        const levelPrice: number[] = [
+            1.8, 1.89, 1.98, 2.07, 2.16, 2.25, 2.34, 2.43, 2.52, 2.61,
+            2.7, 2.79, 2.88, 2.97, 3.06, 3.15, 3.24, 3.33, 3.42, 3.51,
+            3.6, 3.69, 3.78, 3.87, 3.96, 4.05, 4.14, 4.23, 4.32, 4.41,
+            4.5, 4.59, 4.68, 4.77, 4.86, 4.95, 5.04, 5.13, 5.22, 5.31,
+            5.4, 5.49, 5.58, 5.67, 5.76, 5.85, 5.94, 6.03, 6.12, 6.21,
+            6.3, 6.39, 6.48, 6.57, 6.66, 6.75, 6.84, 6.93, 7.02, 7.11,
+            7.2, 7.29, 7.38, 7.47, 7.56, 7.65, 7.74, 7.83, 7.92, 8.01,
+            8.1, 8.19, 8.28, 8.37, 8.46, 8.55, 8.64, 8.73, 8.82, 8.91,
+            9, 9.09, 9.18, 9.27, 9.36, 9.45, 9.54, 9.63, 9.72, 9.81,
+            9.9, 9.99, 10.8, 11.61, 12.42, 13.23, 14.04, 14.85, 16.56, 17.37,
+            18, 20.25, 22.05, 23.49, 24.48, 25.29, 26.1, 27, 27.9, 28.8,
+            29.7, 30.6, 31.5, 32.4, 33.3, 34.2, 35.1, 36, 36.9, 37.8
+        ];
+
+        return levelPrice[level]
 
     }
 
     return(
-        <div>
-            <button onClick={() => connect()}>Connect</button>
-            <br/>
-            <label>{address}</label>
+        <Page>
+            <div className="container mt-24">
+                <button onClick={() => connect()}>Connect</button>
+                <br/>
+                <label>{address}</label>
 
-            <hr/>
+                <hr/>
 
-            <label>{totalValue}$</label><br/>
-            <label>Current level: {currentLevel}/120</label><br/>
-            <label>{totalCurrentLevel}$</label><br/>
+                <label>Current level: {currentLevel+1}/120</label><br/>
+                <label>PFX level price: {getLevelPrice(currentLevel)}$</label><br/>
+                <label>Level Progression: {currentLevelSoldAmountUsd}$</label><br/>
 
-            <hr/>
+                <hr/>
 
-            <button onClick={() => buy()}>Buy</button>
-        </div>
+                <label>{userBalance} BNB </label>
+                <button className="border-2 border-blue-50 disabled:bg-blue disabled:text-white bg-white text-blue" disabled={ !connected } onClick={() => setMaxAllocation()}> 
+                    MAX
+                </button><br/>
+
+                <label>Value in BNB </label>
+                <input disabled={ !connected } value={userAllocation} onChange={(event) => userAllocationChange(event.currentTarget.value)}></input> {` <=> ~`}
+                <label>{PFXtoReceive}PFX</label><br/>
+
+                <label>Referred Address (optional) </label>
+                <input onChange={(event) => setReferrerAddress(event.currentTarget.value)}></input>
+                <br/>
+
+                <button 
+                 className="border-2 border-blue-50 disabled:bg-blue disabled:text-white bg-white text-blue" onClick={() => buy()}
+                 disabled={ !connected || userAllocation === "" || userAllocation === "0" }
+                >
+                    Buy
+                </button>
+
+            </div>
+        </Page>
+        
     )
 
 }
