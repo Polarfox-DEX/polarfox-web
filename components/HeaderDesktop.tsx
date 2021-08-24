9477547c9dd592562a23c7ac5a82cd31c419617c
Clément Aguilar <clem@polarfox.io>
2021-08-24 18:02:07 +0200

    Refactor HeaderDesktop code

--- components/HeaderDesktop.tsx
+++ components/HeaderDesktop.tsx
@@ -28,20 +28,20 @@ export function HeaderDesktop({ className }: SectionProps) {
         </a>
       </Link>
       <div
-        className="grid grid-flow-col gap-x-2 items-center justify-end text-center font-semibold"
+        className="grid grid-flow-col gap-x-2 items-center justify-end font-semibold gap-x-14"
         style={{
           fontSize: calcRem(14)
         }}
       >
         <ProductsDropdown />
         <Link href={ ABOUT_LINK }>
-          <a className="px-6">About</a>
+          <a>About</a>
         </Link>
         <Link href={ ROADMAP_LINK }>
-          <a className="px-6">Roadmap</a>
+          <a>Roadmap</a>
         </Link>
         <Link href={ FAQ_LINK }>
-          <a className="px-6">FAQ</a>
+          <a>FAQ</a>
         </Link>
         <RessourceDropdown />
         <LanguageDropdown />
@@ -72,7 +72,7 @@ function Dropdown({
   return (
     <div className="group relative">
       <div
-        className='grid items-center group-hover:bg-gray-mid2 rounded-3xl px-6 py-4 gap-x-2'
+        className='grid items-center group-hover:bg-gray-mid2 rounded-3xl py-4 gap-x-2'
         style={{ gridTemplateColumns: '0.9fr 0.1fr' }}
       >
         <span className="flex justify-items items-center gap-x-2">{logo} {name}</span>
@@ -183,7 +183,7 @@ function MenuItem({ href, linkTitle, children }: MenuItemProps) {
   return (
     <a
       href={href}
-      className="rounded-2xl py-4 hover:bg-gray-mid2 w-full text-left pl-4 hover:cursor-pointer"
+      className="rounded-2xl py-4 hover:bg-gray-mid2 w-full pl-4 hover:cursor-pointer"
     >
       {children}
     </a>
@@ -212,7 +212,7 @@ function Product({ logo, href, linkTitle, children }: ProductProps) {
         >
           {logo}
           <div
-            className="absolute self-end text-left font-semibold"
+            className="absolute self-end font-semibold"
             style={{
               fontSize: calcRem(12),
               lineHeight: calcRem(14),
@@ -273,7 +273,7 @@ function Language({ href, children }: LanguageProps) {
   return (
     <Link href={href}>
       <a
-        className="grid auto-rows-fr items-center text-left rounded-xl hover:bg-gray-dark p-3 pt-4 font-semibold w-full"
+        className="grid auto-rows-fr items-center rounded-xl hover:bg-gray-dark p-3 pt-4 font-semibold w-full"
         style={{
           width: calcRem(137),
           height: calcRem(40),
