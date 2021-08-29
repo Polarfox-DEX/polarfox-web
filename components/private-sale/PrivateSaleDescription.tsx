153d7697c6d7e22dc6ce9f4916e6d43953dc7ea3
Clément Aguilar <36085723+Klemah@users.noreply.github.com>
2021-08-29 10:28:19 +0200

    Add responsive for private sale page (#26)

--- components/private-sale/PrivateSaleDescription.tsx
+++ components/private-sale/PrivateSaleDescription.tsx
@@ -4,7 +4,7 @@ import { SectionProps } from '../sections/utils/SectionProps'
 
 export function PrivateSaleDescription({ className }: SectionProps) {
   return (
-    <div className={classNames(className)}>
+    <div className={classNames('container', className)}>
       <div
         className="opacity-50"
         style={{
@@ -16,9 +16,8 @@ export function PrivateSaleDescription({ className }: SectionProps) {
         PRIVATE SALE
       </div>
       <h2
-        className="font-switzer font-semibold mt-1"
+        className="description font-switzer font-semibold mt-1"
         style={{
-          width: calcRem(408),
           fontSize: calcRem(50),
           lineHeight: calcRem(65)
         }}
@@ -26,35 +25,30 @@ export function PrivateSaleDescription({ className }: SectionProps) {
         Private PFX sale
       </h2>
       <p
-        className="mt-12 text-gray"
+        className="description mt-12 text-gray"
         style={{
-          width: calcRem(503),
           fontSize: calcRem(20),
           lineHeight: calcRem(32)
         }}
       >
         The PFX private sale will last until{' '}
-        <span className="font-bold text-blue">$1,000,000</span> are raised. The token is
-        sold at a price of $1 per token, on a{' '}
-        <span className="font-bold text-blue">first-come, first-served</span> basis.
+        <span className="font-bold text-blue">$1,000,000</span> are raised. The
+        token is sold at a price of $1 per token, on a{' '}
+        <span className="font-bold text-blue">first-come, first-served</span>{' '}
+        basis.
       </p>
       <p
-        className="mt-10 text-gray"
+        className="description mt-10 text-gray"
         style={{
-          width: calcRem(503),
           lineHeight: calcRem(30)
         }}
       >
-        The price of $1 is <span className="font-bold text-blue">half of the start</span>{' '}
-        ICO price, and 1/40th of the potential end price — a{' '}
+        The price of $1 is{' '}
+        <span className="font-bold text-blue">half of the start</span> ICO
+        price, and 1/40th of the potential end price — a{' '}
         <span className="font-bold text-blue">potential 4000% gain</span>.
       </p>
-      <div
-        className="mt-10 border border-blue bg-white rounded-lg h-40"
-        style={{
-          width: calcRem(503)
-        }}
-      >
+      <div className="description mt-10 border border-blue bg-white rounded-lg">
         <p
           className="text-gray italic mx-6 my-9"
           style={{
@@ -67,9 +61,8 @@ export function PrivateSaleDescription({ className }: SectionProps) {
         </p>
       </div>
       <p
-        className="mt-10 text-gray"
+        className="description mt-10 text-gray"
         style={{
-          width: calcRem(503),
           lineHeight: calcRem(30)
         }}
       >
@@ -79,12 +72,24 @@ export function PrivateSaleDescription({ className }: SectionProps) {
           style={{ fontSize: calcRem(15), lineHeight: calcRem(25) }}
         >
           <li>
-            The destination address <span className="font-bold text-blue underline">must</span> be a <span className="font-bold text-blue-light">MetaMask</span> address. This is because
-            most other wallets do not support Avalanche yet.
+            The destination address{' '}
+            <span className="font-bold text-blue underline">must</span> be a{' '}
+            <span className="font-bold text-blue-light">MetaMask</span> address.
+            This is because most other wallets do not support Avalanche yet.
           </li>
-          <li>Due to regulatory concerns, we are unable to accept investments from US and Canadian citizens.</li>
+          <li>
+            Due to regulatory concerns, we are unable to accept investments from
+            US and Canadian citizens.
+          </li>
         </ul>
       </p>
+      <style jsx>{`
+        @media (min-width: 1024px) {
+          .description {
+            width: ${calcRem(503)};
+          }
+        }
+      `}</style>
     </div>
   )
 }
