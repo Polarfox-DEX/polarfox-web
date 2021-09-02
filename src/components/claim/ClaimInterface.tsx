4633fdc9a73f87664e268095f1ae5aa14e498bae
lona9a <adrianbeckdev@gmail.com>
2021-09-02 15:24:37 +0200

    Added base styling to the panel

--- src/components/claim/ClaimInterface.tsx
+++ src/components/claim/ClaimInterface.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react'
+import React, { Children, ReactNode, useEffect, useState } from 'react'
 import Web3 from 'web3'
 
 // Front-end
@@ -15,262 +15,35 @@ import { useWallet } from '../../hooks/useWallet'
 import { usePrivateSale } from '../../hooks/usePrivateSale'
 
 export function ClaimInterface({ className }: SectionProps) {
-  const SYMBOL: string = 'BNB'
-
   const { hasWallet, connected, accounts, balance, chainId, requestConnection, gasPrice } = useWallet()
   const { correctNetwork, currentBnbPrice, isWhitelisted, remaining, boughtAmount, buyTokens, setJustBought } =
     usePrivateSale()
 
-  const [isInvalidAddress, setIsInvalidAddress] = useState<boolean>(false)
-  const [errorInput, setErrorInput] = useState<string>('')
-  const [buySuccessfulMessage, setBuySuccessfulMessage] = useState<string>('')
-  const [purchaseLoading, setPurchaseLoading] = useState<boolean>(false)
-
-  const [userBnbAllowance, setUserBnbAllowance] = useState<string>('0')
-  const [userUsdAllowance, setUserUsdAllowance] = useState<number>(0.0)
-  const [userRecipientAddress, setUserRecipientAddress] = useState<string>('')
-  const [useMyAddress, setUseMyAddress] = useState<boolean>(false)
-
-  const userAllowanceChange = (value: string) => {
-    const val = value == '' ? '0' : value.replace(',', '.')
-    setUserBnbAllowance(val)
-    setUserUsdAllowance(parseFloat(val) * currentBnbPrice)
-  }
-
-  useEffect(() => {
-    // If there are not enough PFX left to sell, write an error message
-    if (balance && connected && userUsdAllowance > remaining) {
-      setErrorInput('Not enough PFX to sell.')
-    }
-    // If the allowance is higher than the user balance, write an error message
-    else if (balance && connected && parseFloat(userBnbAllowance) > balance) {
-      setErrorInput('Insufficent funds.')
-    }
-    // Clear the error message if needed
-    else if (errorInput || !connected) {
-      setErrorInput('')
-    }
-  }, [connected, balance, errorInput, userBnbAllowance, userUsdAllowance, remaining])
-
-  const setMaxUserAllowance = () => {
-    if (connected) {
-      const max = balance ? (balance - 2 * parseFloat(Web3.utils.fromWei(gasPrice))).toString() : '0'
-      setUserBnbAllowance(max)
-      userAllowanceChange(max)
-    }
-  }
-
-  var onUserRecipientAddressChange = (address: string) => {
-    if (address != '') {
-      setUserRecipientAddress(address)
-      const regex = new RegExp('^0x[a-fA-F0-9]{40}$')
-      setIsInvalidAddress(!regex.test(address))
-    } else {
-      setIsInvalidAddress(false)
-      setUserRecipientAddress('')
-    }
-  }
-
-  var resetUIToDefault = () => {
-    setUserBnbAllowance('0')
-    setUserUsdAllowance(0.0)
-    setPurchaseLoading(false)
-  }
-
-  const purchase = () => {
-    setPurchaseLoading(true)
-
-    buyTokens(userBnbAllowance, useMyAddress ? accounts[0] : userRecipientAddress).then((success: boolean) => {
-      // Transaction is successful, show a confirmation message then reset the interface to default
-      if (success) {
-        setBuySuccessfulMessage(
-          'Successfuly bought ' + new Intl.NumberFormat('en-US').format(userUsdAllowance) + ' PFX!'
-        )
-        resetUIToDefault()
-      }
-      // The transaction was not successful
-      else {
-        setPurchaseLoading(false)
-      }
-
-      // Recalculate the values displayed on the UI
-      setJustBought(true)
-    })
-  }
-
-  return (
-    <div className={classNames('sale-modal bg-blue tablet:rounded-3xl text-white w-full bg-stretch pb-8', className)}>
-      <div
-        className="sale-modal container border-blue bg-blue-gray tablet:rounded-t-3xl flex items-center justify-between px-8"
+  
+  // Front-end
+  return(
+    <div className={classNames('claim-modal bg-blue tablet:rounded-3xl text-white w-full bg-stretch pb-8',className)}>
+      <div 
+        className="claim-modal container border-blue bg-blue-light tablet:rounded-t-3xl flex items-center justify:between px-8"
         style={{
-          height: calcRem(90),
+          height: calcRem(127),
           fontSize: calcRem(20)
         }}
-      >
-        <div>
-          {new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD'
-          }).format(remaining)}{' '}
-          remaining
-          <SideText>out of $1,000,000 worth of PFX</SideText>
-        </div>
-        <Clock />
-      </div>
-      <div className="sale-modal text-white py-4 divide-y divide-white divide-opacity-12">
-        <div className="container tablet:px-8 pb-8">
-          <div className="flex mt-4 items-center">
-            <MainText>Buy</MainText>
-            <DownArrow className="mx-3" />
-          </div>
-          <div
-            className={classNames(' mt-7 bg-blue-gray rounded-xl flex justify-between items-center p-6', {
-              'border-2 border-red-error': errorInput
-            })}
-            style={{
-              height: calcRem(87)
-            }}
-          >
-            <div className="font-semibold truncate" style={{ fontSize: calcRem(18) }}>
-              <input
-                className="bg-blue-gray focus:outline-none w-full"
-                value={userBnbAllowance}
-                onChange={(event) => userAllowanceChange(event.currentTarget.value)}
-              />
-              <SideText className="truncate">
-                {'= '}
-                {new Intl.NumberFormat('en-US', {
-                  style: 'currency',
-                  currency: 'USD'
-                }).format(userUsdAllowance)}
-              </SideText>
-            </div>
-            <div className="mt-1 pl-2">
-              <div className="flex justify-between">
-                <div
-                  className="bg-blue-light rounded-3xl font-semibold text-center hover:cursor-pointer hover:bg-white hover:text-blue-light"
-                  style={{
-                    width: calcRem(45),
-                    height: calcRem(24),
-                    paddingTop: calcRem(5),
-                    fontSize: calcRem(10),
-                    marginRight: calcRem(10)
-                  }}
-                  onClick={() => setMaxUserAllowance()}
-                >
-                  MAX
-                </div>
-                <MainText>{SYMBOL}</MainText>
-              </div>
-              <SideText className="mt-2">Balance: {balance ? balance.toFixed(2) : '0.00'}</SideText>
-            </div>
-          </div>
-          <div className="text-red-error mt-2" style={{ fontSize: calcRem(12) }}>
-            {errorInput}
-          </div>
-          <div className="text-center my-2 opacity-40" style={{ fontSize: calcRem(12) }}>
-            You will get
-          </div>
-          <div className={classNames('bg-blue-gray rounded-xl flex justify-between items-center p-6')}>
-            <div className="truncate">{new Intl.NumberFormat('en-US').format(userUsdAllowance)}</div>
-            <MainText className="pl-2">PFX</MainText>
-          </div>
-        </div>
-        <div className="px-8 pt-2">
-          <MainText className="flex mt-4 items-center">Destination address</MainText>
-          <SideText className="mt-2 opacity-95">
-            Make sure to use a <span className="font-bold text-red-error">MetaMask</span> address.
-          </SideText>
-          <div className="mt-4">
-            <input
-              className={classNames('bg-blue-gray focus:outline-none rounded-xl px-6 disabled:opacity-40 w-full', {
-                'border-2 border-red-error': isInvalidAddress
-              })}
-              style={{
-                height: calcRem(45),
-                fontSize: calcRem(12)
-              }}
-              value={userRecipientAddress}
-              onChange={(event) => onUserRecipientAddressChange(event.currentTarget.value)}
-              placeholder="Please enter the receiving address"
-              disabled={useMyAddress}
-            />
-            <div className="text-red-error mt-3" style={{ fontSize: calcRem(12) }}>
-              {isInvalidAddress && 'Invalid address format.'}
-            </div>
-            <div className="mt-4 px-2" style={{ fontSize: calcRem(12), lineHeight: calcRem(18) }}>
-              <div className="flex items-center space-x-2.5 ">
-                <div
-                  className="border-2 rounded-md border-white border-opacity-30 flex justify-center items-center hover:border-blue-light hover:border-opacity-70"
-                  style={{ width: calcRem(22), height: calcRem(22) }}
-                >
-                  <input
-                    type="checkbox"
-                    className="opacity-0 absolute"
-                    style={{ width: calcRem(22), height: calcRem(22) }}
-                    onChange={(event) => setUseMyAddress(event.currentTarget.checked)}
-                  />
-                  <Check
-                    className={classNames(useMyAddress ? 'flex' : 'hidden')}
-                    style={{ width: calcRem(12), height: calcRem(12) }}
-                  />
-                </div>
-                <div>Send funds to the currently connected address</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="px-8 mt-4">
-          <div className="mb-6 flex">
-            {connected && chainId && (correctNetwork ? <PurchaseButton /> : <WrongNetworkButton />)}
-            {(!connected || !chainId) && <ConnectButton />}
-          </div>
-          <div className="mt-2" style={{ fontSize: calcRem(12) }}>
-            <div className="text-red-error">
-              {connected && !isWhitelisted && correctNetwork && 'Error: Your address is not whitelisted'}
-            </div>
-            <div className="text-green-successful">{buySuccessfulMessage}</div>
-          </div>
-          <div className="mt-6 opacity-40 text-center" style={{ fontSize: calcRem(12), lineHeight: calcRem(18) }}>
-            <span className="font-bold">NOTE: </span>
-            You will start receiving your tokens after the ICO has ended. Since you are participating at a low price,
-            most of your tokens will be vested over time and not given to you at once.
-          </div>
-          <div className="mt-6 font-bold text-center" style={{ fontSize: calcRem(14), lineHeight: calcRem(18) }}>
-            Your total funds in this private sale:{' '}
-            {new Intl.NumberFormat('en-US', {
-              style: 'currency',
-              currency: 'USD'
-            }).format(boughtAmount)}
-          </div>
+    >
+        <div className="claim-modal text-white">
+          <HeadText>Claim your Tokens</HeadText>
         </div>
       </div>
       <style jsx>{`
         @media (min-width: 640px) {
-          .sale-modal {
+          .claim-modal {
             width: ${calcRem(439)};
           }
         }
       `}</style>
     </div>
   )
-
-  function PurchaseButton() {
-    return (
-      <ActionButton
-        disabled={
-          !isWhitelisted ||
-          userBnbAllowance == '0' ||
-          (!useMyAddress && userRecipientAddress === '') ||
-          isInvalidAddress
-        }
-        click={() => purchase()}
-      >
-        {!purchaseLoading && 'Purchase'}
-        {purchaseLoading && 'Please wait...'}
-      </ActionButton>
-    )
-  }
+  
 
   interface ConnectButton {
     hasWallet: boolean
@@ -373,3 +146,24 @@ function MainText({ className, children }: MainTextProps) {
     </div>
   )
 }
+
+interface HeadTextProps {
+  className?: string
+  children: ReactNode
+}
+
+function HeadText({className, children}: HeadTextProps){
+  return (
+    <div
+    className={classNames('font-semibold', className)}
+    style={{
+      fontSize: calcRem(25),
+      lineHeight: calcRem(29)
+    }}
+    >
+      {children}
+    </div>
+  )
+}
+
+
