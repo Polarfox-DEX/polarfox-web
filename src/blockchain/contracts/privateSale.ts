cc051d6bf529f883f1bb5350cf26349974ff9a1e
Clément Aguilar <36085723+Klemah@users.noreply.github.com>
2021-09-04 13:04:22 +0200

    Add isSaleActive management / production contract (#32)
    * Update testnet private sale contract
    
    * Update private sale contract
    
    * Add isSaleActive management
    
    * Add private sale on production (#31)
    
    * Fix typos
    
    * Update whitelisted error message
    
    Co-authored-by: BowgartField <BowgartField@users.noreply.github.com>

--- src/blockchain/contracts/privateSale.ts
+++ src/blockchain/contracts/privateSale.ts
@@ -4,8 +4,8 @@ import { ChainId } from '../const'
 import PolarfoxPrivateSale from '@polarfox/presale/artifacts/contracts/PolarfoxPrivateSale.sol/PolarfoxPrivateSale.json'
 
 export const PRIVATE_SALE_ADDRESS: { [chainId in ChainId]: string } = {
-  [ChainId.BSC]: '',
-  [ChainId.BSC_TESTNET]: '0x4Cd53bC4D513ab94639C5897fbE967EF65e27d6A',
+  [ChainId.BSC]: '0xF35Ce561EdBEbb1ADe64770405B5B1575d76E559',
+  [ChainId.BSC_TESTNET]: '0x1D6e213bEB84f351eAA3354AE2F63Dc87A205953',
   [ChainId.AVALANCHE]: '',
   [ChainId.FUJI]: ''
 }
